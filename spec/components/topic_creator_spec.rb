2e1db6d3edd8beccff236267ce4c7ba262645a46
pirhoo <hello@pirhoo.com>
2026-08-13 07:47:14 +0000

    fix: restore compatibility with Discourse core 2026.7

--- spec/components/topic_creator_spec.rb
+++ spec/components/topic_creator_spec.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
-require 'rails_helper'
+require "rails_helper"
 
 describe TopicCreator do
   fab!(:admin)
 
   let(:valid_attrs) { Fabricate.attributes_for(:topic) }
 
-  describe '#create' do
-    context 'with successful topic creation' do
+  describe "#create" do
+    context "with successful topic creation" do
       before do
         TopicCreator.any_instance.expects(:save_topic).returns(true)
-        TopicCreator.any_instance.expects(:watch_topic).returns(true)
+        TopicCreator.any_instance.expects(:set_author_notification_level).returns(true)
         SiteSetting.allow_duplicate_topic_titles = true
       end
 
@@ -27,11 +27,11 @@ describe TopicCreator do
       end
 
       it "supports all datashare custom_fields" do
-        custom_fields = { 
-          datashare_document_id: "1234", 
-          datashare_document_index: "banana-papers", 
+        custom_fields = {
+          datashare_document_id: "1234",
+          datashare_document_index: "banana-papers",
           datashare_document_routing: "5678",
-          datashare_document_title: "My Document"
+          datashare_document_title: "My Document",
         }
         opts = valid_attrs.merge(custom_fields:)
 
